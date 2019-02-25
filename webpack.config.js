5f2fdd4c3af13a81c92f7004b35a059a09e9a5ee
Micah Stubbs <micah.stubbs@gmail.com>
2019-02-24 19:07:53 -0800

    remove graphql comments for clarity

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,10 @@
 const config = {
   mode: 'production', // "production" | "development" | "none"
   resolve: {
-    // .mjs needed for https://github.com/graphql/graphql-js/issues/1272
     extensions: ['*', '.mjs', '.js', '.json']
   },
   module: {
     rules: [
-      // fixes https://github.com/graphql/graphql-js/issues/1272
       {
         test: /\.mjs$/,
         include: /node_modules/,
